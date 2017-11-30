cb436c331dda32652e3f118f7bcc787679f6f587
Euab <euanmills26@gmail.com>
2017-11-30 20:52:06 +0000

    Aha good pun

--- cogs/memeism.py
+++ cogs/memeism.py
@@ -6,13 +6,14 @@ from discord.ext import commands
 MEMEISM_SERVER_ID = 377838042905313311
 MEMEISM_BOT_ROLE_ID = 385114533284544512
 MEMEISM_FOLLOWER_ROLE_ID = 377839337158475788
+MEMEISM_MAIN_CHANNEL_ID = 377838043345977355
 
 class MemeismExclusive:
 
     def __init__(self, bot):
         self.bot = bot
 
-    async def on_memeber_join(self, member):
+    async def on_member_join(self, member):
         if member.guild.id is not MEMEISM_SERVER_ID:
             return
 
