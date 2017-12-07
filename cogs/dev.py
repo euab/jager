ae1a61f49b5c7884295958583fe50932ba4520b9
Euab <euanmills26@gmail.com>
2017-12-07 21:50:18 +0000

    Added plugin reloader

--- cogs/dev.py
+++ cogs/dev.py
@@ -5,6 +5,9 @@ from discord.ext import commands
 
 log = logging.getLogger('discord')
 
+SUCCESS = '\u2705'
+FAILIURE = '\u2049'
+
 class Dev:
 
     def __init__(self, bot):
@@ -12,29 +15,45 @@ class Dev:
 
     @commands.is_owner()
     @commands.command(pass_context=True, no_pm=True)
-    async def update_listening(self, ctx, *, listening: str):
-        game = discord.Game(name=listening, type=2)
-        await self.bot.change_presence(game=game)
+    async def listening(self, ctx, *, listening: str):
+        try:
+            game = discord.Game(name=listening, type=2)
+            await self.bot.change_presence(game=game)
+            await ctx.message.add_reaction(SUCCESS)
+        except Exception as e:
+            log.error(e)
+            await ctx.message.add_reaction(FAILIURE)
 
     @commands.is_owner()
     @commands.command(hidden=True)
     async def load(self, ctx, *, module):
         try:
             self.bot.load_extension(module)
-            await ctx.message.add_reaction('\u2705')
+            await ctx.message.add_reaction(SUCCESS)
         except Exception as e:
             log.error(e)
-            await ctx.message.add_reaction('\u2049')
+            await ctx.message.add_reaction(FAILIURE)
 
     @commands.is_owner()
     @commands.command(hidden=True)
     async def unload(self, ctx, *, module):
         try:
             self.bot.unload_extension(module)
-            await ctx.message.add_reaction('\u2705')
+            await ctx.message.add_reaction(SUCCESS)
         except Exception as e:
             log.error(e)
-            await ctx.message.add_reaction('\u2049')
+            await ctx.message.add_reaction(FAILIURE)
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def reload(self, ctx, *, module):
+        try:
+            self.bot.unload_extension(module)
+            self.bot.load_extension(module)
+            await ctx.message.add_reaction(SUCCESS)
+        except Exception as e:
+            log.error(e)
+            await ctx.message.add_reaction(FAILIURE)
 
 def setup(bot):
     cog = Dev(bot)
--- cogs/search.py
+++ cogs/search.py
@@ -1,3 +1,4 @@
+import os
 import aiohttp
 import discord
 import secrets
