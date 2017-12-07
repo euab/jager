b773555bf8be6df74638767dde12ce766cf2f738
Euab <euanmills26@gmail.com>
2017-12-07 18:01:01 +0000

    Adding dev module with stuff for me

---
+++ cogs/dev.py
@@ -0,0 +1,18 @@
+import discord
+
+from discord.ext import commands
+
+class Dev:
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.is_owner()
+    @commands.command(pass_context=True, no_pm=True)
+    async def update_listening(self, ctx, *, listening: str):
+        game = discord.Game(name=listening, type=2)
+        await self.bot.change_presense(game=game)
+
+def setup(bot):
+    cog = Dev(bot)
+    bot.add_cog(Dev)
