adfd191fec99a8d104fdaa3c17298fa4dea52fc2
Euab <euanmills26@gmail.com>
2017-12-07 18:58:41 +0000

    Added hot-swapping for cogs

--- cogs/dev.py
+++ cogs/dev.py
@@ -1,6 +1,9 @@
 import discord
+import logging
 
 from discord.ext import commands
+
+log = logging.getLogger('discord')
 
 class Dev:
 
@@ -12,6 +15,26 @@ class Dev:
     async def update_listening(self, ctx, *, listening: str):
         game = discord.Game(name=listening, type=2)
         await self.bot.change_presense(game=game)
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def load(self, ctx, *, module):
+        try:
+            self.bot.load_extension(module)
+            await ctx.message.add_reaction('\u2705')
+        except Exception as e:
+            log.error(e)
+            await ctx.message.add_reaction('\u2049')
+
+    @commands.is_owner()
+    @commands.command(hidden=True)
+    async def unload(self, ctx, *, module):
+        try:
+            self.bot.unload_extension(module)
+            await ctx.message.add_reaction('\u2705')
+        except Exception as e:
+            log.error(e)
+            await ctx.message.add_reaction('\u2049')
 
 def setup(bot):
     cog = Dev(bot)
