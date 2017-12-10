8fe91e460dfa6741f13c1edf72ca2e63665baaa6
Euab <euanmills26@gmail.com>
2017-12-10 14:01:45 +0000

    Just for a bit

--- rickbot.py
+++ rickbot.py
@@ -73,18 +73,7 @@ class RickBot(commands.AutoShardedBot):
 		os.execv(sys.executable, ["python"] + sys.argv)
 
 	async def get_prefix(self, message):
-		with open('data/guild.json') as f:
-			data = json.load(f)
-
-		id = str(getattr(message.guild, 'id', None))
-
-		prefixes = [
-			f'<@{self.user.id}> ',
-			f'<@!{self.user.id}> ',
-			data.get(id, '!')
-			]
-
-		return prefixes
+		return '!'
 
 	async def on_connect(self):
 		log.info("Connected to gateway")
