8b5c23e31c4ccb5bd18a645e2bddcbfa4d333487
Euab <euanmills26@gmail.com>
2017-12-18 20:29:11 +0000

    Adding some docstrings

--- rickbot.py
+++ rickbot.py
@@ -13,122 +13,128 @@ import json
 from collections import defaultdict
 from ext.context import LeContext
 from discord.ext import commands
-from datadog import DDAgent
 
 log = logging.getLogger('discord')
 
+
 class RickBot(commands.AutoShardedBot):
+    """Modified commands.AutoShardedBot class"""
 
-	developers = [
-		'337333673781100545'
-		]
+    def __init__(self):
+        super().__init__(command_prefix=None)
+        self.session = aiohttp.ClientSession(loop=self.loop)
+        self.uptime = datetime.datetime.utcnow()
+        self.commands_used = defaultdict(int)
+        self.process = psutil.Process()
+        self.messages_sent = 0
+        self.load_extensions()
+        self._add_commands()
+        self.remove_command('help')
 
-	def __init__(self):
-		super().__init__(command_prefix=None)
-		self.session = aiohttp.ClientSession(loop=self.loop)
-		self.uptime = datetime.datetime.utcnow()
-		self.commands_used = defaultdict(int)
-		self.process = psutil.Process()
-		self.messages_sent = 0
-		self.load_extensions()
-		self._add_commands()
-		self.remove_command('help')
-		self.dd_agent_url = os.getenv("DD_AGENT_URL") or secrets.DD_AGENT_URL
-		self.stats = DDAgent(self.dd_agent_url)
+    def _add_commands(self):
+        """Adds commands automatically"""
+        for attr in inspect.getmembers(self):
+            if isinstance(attr, commands.Command):
+                self.add_command(attr)
 
-	def _add_commands(self):
-		'''Adds commands automatically'''
-		for attr in inspect.getmembers(self):
-			if isinstance(attr, commands.Command):
-				self.add_command(attr)
+    def load_extensions(self, cogs=None, path='cogs.'):
+        """Load cogs into the bot."""
+        base_extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
+        for extension in cogs or base_extensions:
+            try:
+                self.load_extension(f'{path}{extension}')
+                log.info(f'Loaded extension {extension}')
 
+            except Exception as e:
+                log.info(f'Unable to load extension {extension}. Err: {e}')
+                traceback.print_exc()
 
-	def load_extensions(self, cogs=None, path='cogs.'):
-		base_extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
-		for extension in cogs or base_extensions:
-			try:
-				self.load_extension(f'{path}{extension}')
-				log.info(f'Loaded extension {extension}')
+    @property
+    def token(self):
+        token = os.getenv("TOKEN") or secrets.TOKEN
+        return token
 
-			except Exception as e:
-				log.info(f'Unable to load extension {extension}. Err: {e}')
-				traceback.print_exc()
+    @classmethod
+    def init(bot, token=None):
+        """Start up the bot"""
+        bot = RickBot()
+        token = token or bot.token
+        try:
+            bot.run(token.strip('"'), bot=True, reconnect=True)
+        except Exception as e:
+            log.critical("COULD NOT ACCESS TOKEN!")
+            log.critical(e)
 
-	@property
-	def token(self):
-		token = os.getenv("TOKEN") or secrets.TOKEN
-		return token
+    async def get_prefix(self, message):
+        """Get the prefix for the context's guild"""
+        with open('data/guild.json') as f:
+            data = json.load(f)
 
-	@classmethod
-	def init(bot, token=None):
-		bot = RickBot()
-		token = token or bot.token
-		try:
-			bot.run(token.strip('"'), bot=True, reconnect=True)
-		except Exception as e:
-			log.critical("COULD NOT ACCESS TOKEN!")
-			log.critical(e)
+        gid = str(getattr(message.guild, 'id', None))
 
-	async def get_prefix(self, message):
-		with open('data/guild.json') as f:
-			data = json.load(f)
+        prefixes = [
+            f'<@{self.user.id}> ',
+            f'<@!{self.user.id}> ',
+            data.get(gid, '!')
+        ]
 
-		gid = str(getattr(message.guild, 'id', None))
+        return prefixes
 
-		prefixes = [
-			f'<@{self.user.id}> ',
-			f'<@!{self.user.id}> ',
-			data.get(gid, '!')
-			]
+    async def on_connect(self):
+        """Event triggered when the bot connects to Discord"""
+        log.info("Connected to gateway")
 
-		return prefixes
+    async def on_shard_connect(self, shard_id):
+        """Event triggered when each sharded instance connects"""
+        log.info(f"Shard: {shard_id} connected... ")
 
-	async def on_connect(self):
-		log.info("Connected to gateway")
+    async def on_ready(self):
+        """Triggered when the bot has completed startup"""
+        log.info("Ready. Yay.")
+        self.clean_cache_job()
 
-	async def on_shard_connect(self, shard_id):
-		log.info(f"Shard: {shard_id} connected... ")
+    async def on_command(self, ctx):
+        """Triggered whenever a command is invoked"""
+        cmd = ctx.command.qualified_name.replace(' ', '_')
+        log.info("Invoked: {}@{} >> {}".format(
+            ctx.message.author,
+            ctx.guild.name,
+            ctx.command.qualified_name
+        ))
+        self.commands_used[cmd] += 1
 
-	async def on_ready(self):
-		log.info("Ready. Yay.")
-		self.clean_cache_job()
+    async def on_command_error(self, ctx, error):
+        """Whenever a command fails"""
+        log.error('Error processing command')
+        log.error(error)
 
-	async def on_command(self, ctx):
-		cmd = ctx.command.qualified_name.replace(' ', '_')
-		log.info("Invoked: {}@{} >> {}".format(
-			ctx.message.author,
-			ctx.guild.name,
-			ctx.command.qualified_name
-		))
-		self.commands_used[cmd] += 1
-		self.stats.incr('rickbot.commands_invoked')
+    async def process_commands(self, message):
+        """Process the command"""
+        ctx = await self.get_context(message, cls=LeContext)  # Le Meme
+        if ctx.command is None:
+            return
 
-	async def on_command_error(self, ctx, error):
-		log.error('Error processing command')
-		log.error(error)
+        await self.invoke(ctx)
+        log.info('Command invoked')
 
-	async def process_commands(self, message):
-		ctx = await self.get_context(message, cls=LeContext) # Le Meme
-		if ctx.command is None:
-			return
+    async def on_message(self, message):
+        """The bot's actual listener. Triggered whenever any message is sent."""
+        self.messages_sent += 1
+        if message.author.bot:
+            return
+        await self.process_commands(message)
 
-		await self.invoke(ctx)
-		log.info('Command invoked')
+    def clean_cache_job(self):
+        """Job which cleans the bot's cache"""
+        os.system("rm -r cache")
 
-	async def on_message(self, message):
-		# Actual listener
-		self.messages_sent += 1
-		if message.author.bot:
-			return
-		await self.process_commands(message)
-
-	def clean_cache_job(self):
-		os.system("rm -r cache")
 
 def main():
-	token = os.getenv("TOKEN") or secrets.TOKEN
-	logging.basicConfig(level="INFO")
-	RickBot.init(token)
+    """Initialise logger and run init func"""
+    token = os.getenv("TOKEN") or secrets.TOKEN
+    logging.basicConfig(level="INFO")
+    RickBot.init(token)
+
 
 if __name__ == '__main__':
-	main()
+    main()
