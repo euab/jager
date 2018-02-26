aaf103460d81118fff9de81bf5d58518e390da4b
Euab <euanmills26@gmail.com>
2018-02-26 19:50:33 +0000

    Very important

---
+++ cogs/test.py
@@ -0,0 +1,19 @@
+class Test:
+    """
+    This test asserts the sanity of the known universe.
+    That 1 + 1 == 2. If this test fails then we are all
+    proably dead.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    def test(self):
+        assert 1 + 1 == 2
+        assert 1 + 1 == 2
+
+
+def setup(bot):
+    cog = Test(bot)
+    bot.add_listener(cog.test, "on_ready")
+    # bot.load_cog(cog)
