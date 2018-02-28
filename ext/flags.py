3a2fc07cf30574394d1ed028c4c580e8040c99eb
Euab <euanmills26@gmail.com>
2018-02-28 21:33:01 +0000

    Adding startup flags

---
+++ ext/flags.py
@@ -0,0 +1,18 @@
+import argparse
+import logging
+
+def parse_flags():
+    parser = argparse.ArgumentParser(description="Flags to start the bot with.")
+    parser.add_argument('--debug', help="Start the bot into debug mode.", action="store_true")
+
+    try:
+        args = parser.parse_args()
+    except Exception as e:
+        # Logging has not been setup yet. So we can only print.
+        print(e)
+        exit(0)
+
+    if args.debug:
+        logging.basicConfig(level="DEBUG")
+    else:
+        logging.basicConfig(level="INFO")
