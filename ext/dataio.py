cc460c365f0bffe6667e6ee59c8398f4444448d0
Euab <euanmills26@gmail.com>
2018-03-03 20:42:53 +0000

    embarrassing mistake

--- ext/dataio.py
+++ ext/dataio.py
@@ -6,10 +6,10 @@ class DataIO:
     JSON input/output
     """
 
-    def load_json(path):
+    def load_json(path=None):
         with open(path) as f:
             return json.load(f)
 
-    def save_json(path):
+    def save_json(data, path=None):
         with open(path, 'w') as f:
             f.write(json.dumps(data, indent=4))
