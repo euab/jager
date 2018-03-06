a0ec1e2c8e3a06281776df2ee3a8da4146fa5dac
Euab <euanmills26@gmail.com>
2018-03-06 21:43:17 +0000

    Add --log-to-file flag

--- ext/flags.py
+++ ext/flags.py
@@ -5,6 +5,7 @@ def parse_flags():
     parser = argparse.ArgumentParser(description="Flags to start the bot with.")
     parser.add_argument('--debug', help="Start the bot into debug mode.", action="store_true")
     parser.add_argument('--test-run', help="For CI testing", action="store_true")
+    parser.add_argument('--log-to-file', help="Write logs to file", action="store_true")
 
     try:
         args = parser.parse_args()
@@ -14,10 +15,15 @@ def parse_flags():
         exit(0)
 
     if args.debug:
-        logging.basicConfig(level="DEBUG")
-    else:
-        logging.basicConfig(level="INFO")
+        logging.basicConfig(level=logging.DEBUG)
+
+    if args.log_to_file:
+        rickbot_log = logging.basicConfig(filename="log.log",
+                                          filemode="w",
+                                          format="[%(asctime)s] %(msecs)d %(name)s %(levelname)s %(message)s",
+                                          datefmt="'%H:%M:%S'",
+                                          level=logging.DEBUG)
 
     if args.test_run:
-        logging.basicConfig(level="DEBUG")
+        logging.basicConfig(level=logging.DEBUG)
         print("Running for CI")
