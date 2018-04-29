7a417840440a6014b5f73f4d380dfd5d567e2478
euanmills <euanmills26@gmail.com>
2018-04-29 19:11:03 +0100

    Add environment variable for SENTRY_DSN

--- oofster.py
+++ oofster.py
@@ -9,11 +9,11 @@ import logging
 import secrets
 import json
 import asyncio
-import raven
 
 from collections import defaultdict
 from ext.context import LeContext
 from discord.ext import commands
+from raven import client
 
 log = logging.getLogger('discord')
 
@@ -33,7 +33,7 @@ class TheOofster(commands.AutoShardedBot):
         self._add_commands()
         self.loop = asyncio.get_event_loop()
         self.psa = None
-        self.sentry = raven.Client(secrets.SENTRY_DSN)
+        self.sentry = Client(os.getenv("SENTRY_DSN") or secrets.SENTRY_DSN)
 
     def _add_commands(self):
         """Adds commands automatically"""
