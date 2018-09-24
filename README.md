aa9c08952301d2c7416b515d77c72a654aa06797
euanmills <euanmills26@gmail.com>
2018-09-24 21:19:52 +0100

    I'm back
    Well this is a suprise. I'm back. I've changed the name of the bot and made a new VPS.
    The main reason is because it is now winter and I'm going to be spending more time in my house so
    I need to start having things to do so I don't start going mentally insane. I'm probably going to
    change up this bot completly but for the time being I'm just going to run the old code. I might start
    rewriting this in Java. This will likely be followed by other commits as some of the code might have
    broken due to breaking changes or the tweaks that I have made.
    
    I renamed it to Jäger is because I didn't want the name of the bot to be a meme anymore. Because, memes
    move on and so the name will lose its novelty quickly. Why Jäger? Go figure. I'm not telling you.

--- README.md
+++ README.md
@@ -1,16 +1,21 @@
-> **This project has been DEPRECATED.** The code here is now public domain. Do whatever you want with it.
-
-# The Oofster
+# Jäger
+Jäger is a open source bot for Discord. It has many different functionalities, that are stated below.
 
 [![Build Status](https://travis-ci.org/Euab/the-oofster.svg?branch=master)](https://travis-ci.org/Euab/the-oofster) [![PyPI](https://img.shields.io/badge/Python-3.6.3-green.svg)](https://www.python.org/downloads/)  [![Codacy Badge](https://api.codacy.com/project/badge/Grade/a0d19fe4283b4288a5146caa5c0891c5)](https://www.codacy.com/app/Euab/rickbot?utm_source=github.com&amp;utm_medium=referral&amp;utm_content=rickbotdiscord/rickbot&amp;utm_campaign=Badge_Grade)
 
-A cool bot for Discord.
+> Please do not try to host your own instance of this bot, it is built for the environment that it runs in. If you want the bot in your server, visit the website in the repository's description.
 
-Please do not try to host your own instance of this bot. Just invite it by calling the `invite` command via DMs.
+# Getting Started
+Invite the bot to your server by visiting [this link.](https://discordapp.com/oauth2/authorize?&client_id=493534734413201408&scope=bot&permissions=8)
+From here you can call `!help` to see a list of the commands.
 
-## Getting started
-By default. Oofster's prefix is `!`. This can be changed at anytime by writing `!prefix <new prefix>`.
-After that you can invoke commands with the new prefix.
-The prefix can consist of any alphanumeric character.
+> This bot is current in an unstable state. Please do not expect every
+functionality to work. Because it might not.
 
-To get help with a command type `!help <command name>` and the bot will show you the arguments to pass along with the command.
+# Backstory
+This bot used to be called The Oofster. I worked on it for around six months
+where I made many of the plugins that its users enjoyed such as music functions.
+But, due to my now existent social life (ha, get it programmers have no life)
+I didn't have much time to work on the bot. I just became very busy, I still am
+in a way. But, this is just something for me to do in my spare time. Winters are
+usually boring.
---
+++ jager.py
@@ -0,0 +1,181 @@
+import discord
+import datetime
+import traceback
+import aiohttp
+import psutil
+import os
+import inspect
+import logging
+import secrets
+import json
+import asyncio
+
+from collections import defaultdict
+from ext.context import LeContext
+from discord.ext import commands
+from raven import Client
+
+log = logging.getLogger('discord')
+
+
+class Jager(commands.AutoShardedBot):
+    """Modified commands.AutoShardedBot class"""
+
+    def __init__(self):
+        super().__init__(command_prefix=None)
+        self.session = aiohttp.ClientSession(loop=self.loop)
+        self.uptime = datetime.datetime.utcnow()
+        self.commands_used = defaultdict(int)
+        self.commands_failed = defaultdict(int)
+        self.process = psutil.Process()
+        self.messages_sent = 0
+        self.sentry = Client(os.getenv("SENTRY_DSN") or secrets.SENTRY_DSN)
+        self.load_extensions()
+        self._add_commands()
+        self.loop = asyncio.get_event_loop()
+        self.psa = None
+
+    def _add_commands(self):
+        """Adds commands automatically"""
+        for attr in inspect.getmembers(self):
+            if isinstance(attr, commands.Command):
+                self.add_command(attr)
+
+    def load_extensions(self, cogs=None, path='cogs.'):
+        """Load cogs into the bot."""
+        base_extensions = [x.replace('.py', '') for x in os.listdir('cogs') if x.endswith('.py')]
+        for extension in cogs or base_extensions:
+            try:
+                self.load_extension(f'{path}{extension}')
+                log.info(f'Loaded extension {extension}')
+
+            except Exception as e:
+                log.info(f'Unable to load extension {extension}. Err: {e}')
+                traceback.print_exc()
+                self.sentry.captureException()
+
+    @property
+    def token(self):
+        token = os.getenv("TOKEN") or secrets.TOKEN
+        return token
+
+    @classmethod
+    def init(bot, token=None):
+        """Start up the bot"""
+        bot = Jager()
+        token = token or bot.token
+        try:
+            bot.run(token.strip('"'), bot=True, reconnect=True)
+        except Exception as e:
+            log.critical("COULD NOT ACCESS TOKEN!")
+            log.critical(e)
+
+    async def get_prefix(self, message):
+        """Get the prefix for the context's guild"""
+        with open('data/guild.json') as f:
+            data = json.load(f)
+
+        gid = str(getattr(message.guild, 'id', None))
+
+        prefixes = [
+            f'<@{self.user.id}> ',
+            f'<@!{self.user.id}> ',
+            data or "!"
+        ]
+
+        return prefixes
+
+    async def create_activity(self):
+        """Create standard ticker presence"""
+        while True:
+            activity = discord.Game(name="!help | Get help")
+            await self.change_presence(activity=activity)
+            await asyncio.sleep(12.0)
+            activity = discord.Streaming(name="Uncompelling gameplay",
+                                        url="https://www.twitch.tv/euab")
+            await self.change_presence(activity=activity)
+            await asyncio.sleep(12.0)
+            total_online = len({m.id for m in self.get_all_members() if m.status is not discord.Status.offline})
+            total_unique = len(self.users)
+            activity = discord.Activity(name=f"{total_online}/{total_unique} users online", type=3)
+            await self.change_presence(activity=activity)
+            await asyncio.sleep(12.0)
+            vc = len(self.voice_clients)
+            if vc == 1:
+                activity = discord.Activity(name=f"{vc} voice client", type=2)
+            else:
+                activity = discord.Activity(name=f"{vc} voice clients", type=2)
+            await self.change_presence(activity=activity)
+            await asyncio.sleep(12.0)
+
+    async def on_connect(self):
+        """Event triggered when the bot connects to Discord"""
+        log.info("Connected to gateway")
+
+    async def on_shard_connect(self, shard_id):
+        """Event triggered when each sharded instance connects"""
+        log.info(f"Shard: {shard_id} connected... ")
+
+    async def on_ready(self):
+        """Triggered when the bot has completed startup"""
+        log.info("Ready. Yay.")
+        await self.create_activity()
+        self.test_sentry()
+
+        # ready ascii
+        with open("ready_ascii.txt") as f:
+            print(f'\n{f.read()}')
+
+    async def on_command(self, ctx):
+        """Triggered whenever a command is invoked"""
+        cmd = ctx.command.qualified_name.replace(' ', '_')
+        log.info("Invoked: {}@{} >> {}".format(
+            ctx.message.author,
+            ctx.guild.name,
+            ctx.command.qualified_name
+        ))
+        self.commands_used[cmd] += 1
+
+    async def on_command_error(self, ctx, error):
+        """Whenever a command fails"""
+        log.error(error)
+        traceback.print_exc()
+        cmd = ctx.command.qualified_name.replace(' ', '_')
+        self.commands_failed[cmd] += 1
+
+    async def process_commands(self, message):
+        """Process the command"""
+        ctx = await self.get_context(message, cls=LeContext)  # Le Meme
+        if ctx.command is None:
+            return
+
+        await self.invoke(ctx)
+
+    async def on_message(self, message):
+        """The bot's actual listener. Triggered whenever any message is sent."""
+        self.messages_sent += 1
+        if message.author.bot:
+            return
+        await self.process_commands(message)
+
+    def test_sentry(self):
+        """Test Sentry by creating an error and having the DSN capture it"""
+        try:
+            1 / 0
+        except ZeroDivisionError:
+            self.sentry.captureException()
+
+
+def main():
+    """Initialise logger and run init func"""
+    token = os.getenv("TOKEN") or secrets.TOKEN
+    logging.basicConfig(filename="log.log",
+                        filemode="w",
+                        format="[%(asctime)s] %(msecs)d %(name)s %(levelname)s %(message)s",
+                        datefmt="'%H:%M:%S'",
+                        level=logging.INFO)
+    Jager.init(token)
+
+
+if __name__ == '__main__':
+    main()
