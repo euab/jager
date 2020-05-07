6a5b2997b1fdbd1bbf2d19368344adfaa2c7c72e
Euab <euanmills26@gmail.com>
2020-05-07 18:04:27 +0100

    Making humanized time method more legible

--- src/main/java/com/euii/jager/utilities/Time.java
+++ src/main/java/com/euii/jager/utilities/Time.java
@@ -6,36 +6,63 @@ public class Time {
 
     /**
      * Convert a millisecond duration to a human readable datetime, in Dd-Hh-Mm-Ss format.
-     * https://stackoverflow.com/a/7663966
      *
      * @param millis A duration to convert to a human-readable string.
      * @return A string in the format Dd-Hh-Mm-Ss.
      */
     public static String makeHumanReadableTime(long millis) {
-
         if (millis < 0) {
             throw new IllegalArgumentException("Duration must be greater than zero!");
         }
 
         long days = TimeUnit.MILLISECONDS.toDays(millis);
-        millis -= TimeUnit.DAYS.toMillis(days);
-        long hours = TimeUnit.MILLISECONDS.toHours(millis);
-        millis -= TimeUnit.HOURS.toMillis(hours);
-        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis);
-        millis -= TimeUnit.MINUTES.toMillis(minutes);
-        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis);
+        long hours = TimeUnit.MILLISECONDS.toHours(millis) - TimeUnit.DAYS.toHours(TimeUnit.MILLISECONDS.toDays(millis));
+        long minutes = TimeUnit.MILLISECONDS.toMinutes(millis) - TimeUnit.HOURS.toMinutes(TimeUnit.MILLISECONDS.toHours(millis));
+        long seconds = TimeUnit.MILLISECONDS.toSeconds(millis) - TimeUnit.MINUTES.toSeconds(TimeUnit.MILLISECONDS.toMinutes(millis));
 
-        StringBuilder sb = new StringBuilder(64);
-        sb.append(days);
-        sb.append(" Days ");
-        sb.append(hours);
-        sb.append(" Hours ");
-        sb.append(minutes);
-        sb.append(" Minutes ");
-        sb.append(seconds);
-        sb.append(" Seconds ");
+        StringBuilder response = new StringBuilder();
 
-        return sb.toString();
+        boolean leading = false;
+
+        if (days > 0) {
+            response.append(days).append(" ").append((days > 1 ? "days" : "day"));
+            leading = true;
+        }
+
+        if (hours > 0) {
+            if (leading && (minutes != 0 || seconds != 0))
+                response.append(", ");
+
+            if (!response.toString().isEmpty() && (minutes == 0 && seconds == 0))
+                response.append(" and ");
+
+            response.append(hours).append(" ").append((hours > 1 ? "hours" : "hour"));
+            leading = true;
+        }
+
+        if (minutes > 0) {
+            if (leading && seconds != 0)
+                response.append(", ");
+
+            if (!response.toString().isEmpty() && seconds == 0)
+                response.append(" and ");
+
+            leading = true;
+
+            response.append(minutes).append(" ").append((minutes > 1 ? "minutes" : "minute"));
+        }
+
+        if (seconds > 0) {
+            if (leading)
+                response.append(" and ");
+
+            response.append(seconds).append(" ").append((seconds > 1 ? "seconds" : "second"));
+        }
+
+        if (response.toString().isEmpty() && !leading)
+            response.append("0 seconds (just now)...");
+
+        return response.toString();
     }
 
 
